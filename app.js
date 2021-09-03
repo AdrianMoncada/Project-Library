263c18b3e43d3d1acb23723d062f08b86f0ce9c9
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2021-09-03 18:25:44 -0300

    refactoring the app

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
 
-var myLibrary = ["You Don't Know JS","Clean Code"];
+let myLibrary = [
+  { 
+    name: "Flowers for Algernon",
+    author: "Daniel Keyes",
+    pages: "311"
+  },
+  {
+    name: "Ender's Game",
+    author: "Orson Scott Card",
+    pages: "320" 
+  }
+];
 
-function Book() {
-  // the constructor...
-}
-            
-function addBookToLibrary()
-{
-    // get value from the input text
-    var inputText = document.getElementById('inputText').value;
-    
-    // append data to the array
-    myLibrary.push(inputText);
-    
-    var showLibrary = "";
-    
-    for(i = 0; i < myLibrary.length; i++)
-    {
-      showLibrary += myLibrary[i] + "<br/>";
-    }
-    
-    // display array data
-    document.getElementById('pText').innerHTML = showLibrary;
+function Book(name,author,pages) {
+  this.name = name 
+  this.author = author
+  this.pages = pages
 }
 
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
+function addBookToLibrary() {
+  var bookName = document.getElementById('bookName').value;
+  var bookAuthor = document.getElementById('bookAuthor').value;
+  var bookPages = document.getElementById('bookPages').value;
+
+  var book1 = new Book(bookName, bookAuthor, bookPages)
+  myLibrary.push(book1)
 }
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-} 
 
+const book0Name = document.getElementById('book0Name');
+const book0Author = document.getElementById('book0Author');
+const book0Pages = document.getElementById('book0Pages');
+
+function displayBooks() { 
+for (let i = 0; i < myLibrary.length; i++) {
+  if (myLibrary != []) {
+    book0Name.innerText = myLibrary[i].name
+    book0Author.innerText = myLibrary[i].author
+    book0Pages.innerText += myLibrary[i].pages
+    return
+  }
+}
+}
+
+displayBooks()
