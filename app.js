170b1e69e3ab9278ce5424f709e1e55879bceb1d
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2021-09-03 22:26:57 -0300

    making the app work

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ function addBookToList(book) {
   const row = document.createElement('tr');
 
   row.innerHTML = `
-    <td>${book.title}</td>
+    <td>${book.name}</td>
     <td>${book.author}</td>
-    <td>${book.isbn}</td>
+    <td>${book.pages}</td>
     <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
   `;
 
